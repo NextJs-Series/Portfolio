ee26affe91dd8e4210b29b8379888f8d07b0ee8b
Nauman-cloud <naumankhan642@gmail.com>
2021-08-02 17:37:42 +0500

    portfolio ready to show

---
+++ src/constants/constant.js
@@ -0,0 +1,46 @@
+export const projects = [
+    {
+        title: 'Pizza App',
+        description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+        image: '/images/img1.png',
+        tags: ['Mongo', 'Express', 'React', 'Node'],
+        source: 'https://google.com',
+        visit: 'https://60068506a9166ec003632562--blissful-meitner-727e86.netlify.app/',
+        id: 0,
+    },
+    {
+        title: 'My Blog App',
+        description: "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+        image: '/images/img2.png',
+        tags: ['React', 'JavaScript'],
+        source: 'https://google.com',
+        visit: 'https://objective-hugle-d171d3.netlify.app/',
+        id: 1,
+    },
+    {
+        title: 'Memories App',
+        description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+        image: '/images/img3.png',
+        tags: ['React', 'WebRTC'],
+        source: 'https://google.com',
+        visit: 'https://memories-pino.netlify.app/',
+        id: 2,
+    },
+    {
+        title: 'Nauman Keep App',
+        description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
+        image: '/images/img4.png',
+        tags: ['React', 'ChatEngine', 'Firebase'],
+        source: 'https://google.com',
+        visit: 'https://stupefied-leavitt-600991.netlify.app/',
+        id: 3,
+    },
+];
+
+export const TimeLineData = [
+    { year: 2017, text: 'Started my journey', },
+    { year: 2018, text: 'Worked with Java Scripts And C++', },
+    { year: 2019, text: 'Move on React js', },
+    { year: 2020, text: 'Shared my projects with the world', },
+    { year: 2021, text: 'Started my own platform', },
+];
