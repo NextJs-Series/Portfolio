5afdf06da0946742a9395ad8eca133bc3b182f64
Nauman-cloud <naumankhan642@gmail.com>
2021-08-02 19:39:37 +0500

    fix css path in timeline

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -2,7 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 
 import { CarouselButton, CarouselButtonDot, CarouselButtons, CarouselContainer, CarouselItem, CarouselItemImg, CarouselItemText, CarouselItemTitle, CarouselMobileScrollNode } from './TimeLineStyles';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { TimeLineData } from '/home/nauman/portfolio/src/constants/constant.js';
+import { TimeLineData } from '../../constants/constant';
 
 const TOTAL_CAROUSEL_COUNT = TimeLineData.length;
 
